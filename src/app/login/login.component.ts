66069edf1039c0de4511f29aff94b48c04a2d796
Hriday <hridayjadhav789@gmail.com>
2023-09-13 13:38:56 +0530

    Here I have done with the signUp page with validation and data transfering

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,7 +6,7 @@ import {
   FormControl,
 } from '@angular/forms';
 import { Router } from '@angular/router';
-import { contactNoErrors, otpNoErrors, } from '../utils/utils';
+import { addressErrors, contactNo1Errors, contactNoErrors, emailErrors, firstNameErrors, lastNameErrors, otpNoErrors, } from '../utils/utils';
 import { CookieService } from 'ngx-cookie-service';
 import { login_img_url, otp_img } from '../constants/constants';
 
@@ -19,11 +19,15 @@ export class LoginComponent implements OnInit {
   // Login Form data
   loginForm: any;
   mobileNum = '';
+  mobileNum1 = '';     //each thing which have 1 infront of he property or etc, its bcz of signup mobile number
   public isLoggedIn = false;
   // Otp form data
   num = 10;
   interval: any;
   otpForm: any;
+  //sign up data
+  signupForm : any;
+  public isSignUp = false;  
 
   //imgs
   loginImage = login_img_url;
@@ -33,12 +37,29 @@ export class LoginComponent implements OnInit {
   transferNum() {
     const mobileNum = this.loginForm.get('contactNo').value;
   }
+  transferNum1() {
+    const mobileNum1 = this.signupForm.get('contactNo1').value;
+  }
 
   constructor(private fb: FormBuilder, private router: Router, private cookie : CookieService) {}
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
       contactNo: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(10),
+          Validators.maxLength(10),
+        ],
+      ],
+    });
+    this.signupForm = this.fb.group({
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
+      address: ['', [Validators.required, Validators.minLength(10)]],
+      contactNo1: [
         '',
         [
           Validators.required,
@@ -60,6 +81,24 @@ export class LoginComponent implements OnInit {
   getContactNumberErrorMessage(): any {
     return contactNoErrors(this.contactNo.errors);
   }
+  getContact1NumberErrorMessage(): any {
+    return contactNo1Errors(this.contactNo1.errors);
+  }
+
+  getFirstNameErrorMessage(): any {
+    return firstNameErrors(this.firstName.errors);
+  }
+
+  getLastNameErrorMessage(): any {
+    return lastNameErrors(this.lastName.errors);
+  }
+  getEmailErrorMessage():any{
+    return emailErrors(this.email.errors);
+  }
+
+  getAddressErrorMessage():any{
+    return addressErrors(this.address.errors)
+  }
 
   getOtpErrorMessage(): any {
     return otpNoErrors(this.otpNo.errors);
@@ -71,6 +110,19 @@ export class LoginComponent implements OnInit {
     this.cookie.set('isVerified', 'true');
     this.interval = setInterval(() => this.numDecrement(this.num), 1000);
   }
+
+  onSignupSubmit() {
+    const firstName = this.signupForm.get('firstName').value;
+    const lastName = this.signupForm.get('lastName').value;
+    const email = this.signupForm.get('email').value;
+    const address = this.signupForm.get('address').value;
+    this.mobileNum1 = this.signupForm.get('contactNo1').value;
+    this.cookie.set('isVerified', 'true');
+    this.isSignUp = false;
+    this.isLoggedIn = true;
+    this.restartTimer();
+  }
+
   goToHome() {
     const enteredOTP = this.otpForm.get('otpNo').value;
     if (enteredOTP) {
@@ -85,6 +137,12 @@ export class LoginComponent implements OnInit {
       this.contactNo.setValue(this.contactNo.value.slice(0, 10)); //contactNo value aur uski lenth agr 10 se zyada hai, to wo 10 number hi lega aur baki slice krega
     }
   }
+  mobileNumberValidation1() {
+    if (this.contactNo1.value && this.contactNo1.value.length > 10) {
+      //if the number is more than 10, then the slice will show only 10 digits and it will automatically removes remaining digits.
+      this.contactNo1.setValue(this.contactNo1.value.slice(0, 10)); //contactNo value aur uski lenth agr 10 se zyada hai, to wo 10 number hi lega aur baki slice krega
+    }
+  }
   limit() {
     if (this.otpNo.value && this.otpNo.value.length > 4) {
       this.otpNo.setValue(this.otpNo.value.slice(0, 4));
@@ -93,6 +151,21 @@ export class LoginComponent implements OnInit {
 
   get contactNo() {
     return this.loginForm.get('contactNo'); //  input ka pura data including validation wo return krega.
+  }
+  get contactNo1() {
+    return this.signupForm.get('contactNo1'); //  input ka pura data including validation wo return krega.
+  }
+  get firstName(){
+    return   this.signupForm.get("firstName");//input ka pura data including validation wo return krega.
+  }
+  get lastName(){
+    return this.signupForm.get('lastName');
+  }
+  get email (){
+    return this.signupForm.get('email');
+  }
+  get address (){
+    return this.signupForm.get('address');
   }
   get otpNo() {
     return this.otpForm.get('otpNo'); //  input ka pura data including validation wo return krega.
@@ -113,6 +186,14 @@ export class LoginComponent implements OnInit {
   
   public backToLogin() {
     this.isLoggedIn = false;
+    this.isSignUp = false;
+  }
+  public backToSignup() {
+    this.isSignUp = true;
+  }
+
+  switchToSignUp() {
+    this.isSignUp = true;
   }
   
 
--- src/app/utils/utils.ts
+++ src/app/utils/utils.ts
@@ -22,28 +22,60 @@ export function otpNoErrors(errors: any): any {
   }
   return ''; // Return an empty string if no errors are found
 }
-// export function firstNameErrors(errors: any): any {
-//   if (errors.required) {
-//     return 'Please enter your First Name';
-//   }
-//   if (errors.minlength) {
-//     return 'Please enter your First Name';
-//   }
-//   if (errors.maxlength) {
-//     return 'Please enter your First Name';
-//   }
-//   return '';
-// }
+export function firstNameErrors(errors: any): any {
+  if (errors.required) {
+    return 'Please enter your First Name';
+  }
+  if (errors.minlength) {
+    return 'Please enter your First Name';
+  }
+  if (errors.maxlength) {
+    return 'Please enter your First Name';
+  }
+  return '';
+}
 
-// export function lastNameErrors(errors: any): any {
-//   if (errors.required) {
-//     return 'Please enter your Last Name';
-//   }
-//   if (errors.minlength) {
-//     return 'Please enter your Last Name';
-//   }
-//   if (errors.maxlength) {
-//     return 'Please enter your Last Name';
-//   }
-//   return '';
-// }
+export function lastNameErrors(errors: any): any {
+  if (errors.required) {
+    return 'Please enter your Last Name';
+  }
+  if (errors.minlength) {
+    return 'Please enter your Last Name';
+  }
+  if (errors.maxlength) {
+    return 'Please enter your Last Name';
+  }
+  return '';
+}
+
+export function emailErrors(errors: any): any {
+  if (errors.required) {
+    return 'Please enter your Email';
+  }
+  if(errors.pattern){
+    return "Enter a valid Email";
+  }
+  return '';
+}
+
+export function addressErrors(errors: any): any {
+  if (errors.required) {
+    return 'Please enter your Address';
+  }
+  if(errors.minlength){
+    return 'Please enter a valid Address'
+  }
+  return '';
+}
+export function contactNo1Errors(errors: any): any {
+  if (errors.required) {
+    return 'Please Enter A Valid Mobile Number';
+  }
+  if (errors.minlength) {
+    return 'Please Enter A Valid Mobile Number (10 digits)';
+  }
+  if (errors.maxlength) {
+    return 'Please Enter A Valid Mobile Number (10 digits)';
+  }
+  return '';
+}
